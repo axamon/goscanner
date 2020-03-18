35fe121ba1bab705a6b768231f2193dc966eff92
00246506 <alberto.bregliano@telecomitalia.it>
2020-03-18 13:59:47 +0100

    Version: v0.2.11

--- compile.bat
+++ compile.bat
@@ -8,16 +8,16 @@ rem Se ci sono problemi esce senza proseguire con la build.
 go test -v -cover -o goscannerTest.exe || goto exit
 
 set GOOS=openbsd
-go build .\cmd -ldflags="-n -s -X 'main.Version=%ver%'" -o .\eseguibili\goscanner-openbsd
+go build .\cmd -ldflags="-n -s -X 'main.Version=%ver%'" -o eseguibili\goscanner-openbsd
 
 set GOOS=linux
-go build .\cmd -ldflags="-n -s -X 'main.Version=%ver%'" -o .\eseguibili\goscanner-linux
+go build .\cmd -ldflags="-n -s -X 'main.Version=%ver%'" -o eseguibili\goscanner-linux
 
 set GOOS=darwin
-go build .\cmd -ldflags="-n -s -X 'main.Version=%ver%'" -o .\eseguibili\goscannermac
+go build .\cmd -ldflags="-n -s -X 'main.Version=%ver%'" -o eseguibili\goscannermac
 
 set GOOS=windows
-go build .\cmd -ldflags="-n -s -X 'main.Version=%ver%'" -o .\eseguibili\goscanner.exe
+go build .\cmd -ldflags="-n -s -X 'main.Version=%ver%'" -o eseguibili\goscanner.exe
 
 git checkout -b %ver%
 git add .
